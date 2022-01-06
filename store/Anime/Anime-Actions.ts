122b421f22e2f429b9f7b0e8a7d70e60d2922584
trickkyricky <rickygyoungjr@gmail.com>
2022-01-06 17:27:08 -0600

    Redux store slices complete along with function tweaks

--- store/Anime/Anime-Actions.ts
+++ store/Anime/Anime-Actions.ts
@@ -1,44 +1,75 @@
-import { AnimeActions } from './Anime-Slice';
+import { AnimeActions } from "./Anime-Slice";
 
-const CLIENT_ID = '4b75af5e9466c3b143191b860c40c4eb';
+const CLIENT_ID = "4b75af5e9466c3b143191b860c40c4eb";
 
-export const getData = (type: string) => {
-	return async (dispatch: any) => {
-		dispatch(AnimeActions.setLoader({ choice: type, loading: true }));
+export const getAnimeData = (type: string) => {
+  return async (dispatch: any) => {
+    dispatch(AnimeActions.setLoader({ choice: type, loading: true }));
 
-		const getData = async () => {
-			const res = await fetch(
-				`https://api.myanimelist.net/v2/anime/ranking?ranking_type=${type}`,
-				{
-					headers: { 'X-MAL-Client-ID': CLIENT_ID }
-				}
-			);
-			if (res.status !== 200) {
-				throw new Error(`Failed to fetch Top ${type}`);
-			}
-			return res.json();
-		};
+    const getData = async () => {
+      const res = await fetch(
+        `https://api.myanimelist.net/v2/anime/ranking?ranking_type=${type}`,
+        {
+          headers: { "X-MAL-Client-ID": CLIENT_ID },
+        }
+      );
+      if (res.status !== 200) {
+        throw new Error(`Failed to fetch Top ${type}`);
+      }
+      return res.json();
+    };
 
-		try {
-			const result = await getData();
-			// switch (type) {
-			// 	case 'airing':
-			// 		dispatch(AnimeActions.setTopAiring(result.data))
-			// 		break;
-			// 	case 'upcoming':
-			// 		dispatch(AnimeActions.setTopUpcoming(result.data));
-			// 		break;
-			// 	case 'movie':
-			// 		dispatch(AnimeActions.setTopMovies(result.data));
-			// 		break;
-			// 	case 'bypopularity':
-			// 		dispatch(AnimeActions.setMostPopular(result.data));
-			// 		break;
-			// }
-			dispatch(AnimeActions.setLoader({ choice: type, loading: false }));
-		} catch (err) {
-			dispatch(AnimeActions.setLoader({ choice: type, loading: false }));
-			console.log(err);
-		}
-	};
+    try {
+      const result = await getData();
+      console.log(result.data);
+      // switch (type) {
+      // 	case 'airing':
+      // 		dispatch(AnimeActions.setTopAiring(result.data))
+      // 		break;
+      // 	case 'upcoming':
+      // 		dispatch(AnimeActions.setTopUpcoming(result.data));
+      // 		break;
+      // 	case 'movie':
+      // 		dispatch(AnimeActions.setTopMovies(result.data));
+      // 		break;
+      // 	case 'bypopularity':
+      // 		dispatch(AnimeActions.setMostPopular(result.data));
+      // 		break;
+      // }
+      dispatch(AnimeActions.setLoader({ choice: type, loading: false }));
+    } catch (err) {
+      dispatch(AnimeActions.setLoader({ choice: type, loading: false }));
+      console.log(err);
+    }
+  };
 };
+
+export const getSeasonalAnimeData = (season: string, year: string) => {
+  return async (dispatch: any) => {
+    dispatch(AnimeActions.setLoader({ choice: "season", loading: true }));
+
+    const getData = async () => {
+      const res = await fetch(
+        `https://api.myanimelist.net/v2/anime/season/${year}/${season}`,
+        {
+          headers: { "X-MAL-Client-ID": CLIENT_ID },
+        }
+      );
+      if (res.status !== 200) {
+        throw new Error(`Failed to fetch Top Seasonal ${season} Anime`);
+      }
+      return res.json();
+    };
+
+    try {
+      const result = await getData();
+      dispatch(AnimeActions.setTopSeasonal(result.data));
+      dispatch(AnimeActions.setLoader({ choice: "season", loading: false }));
+    } catch (err) {
+      dispatch(AnimeActions.setLoader({ choice: "season", loading: false }));
+      console.log(err);
+    }
+  };
+};
+
+// `https://api.myanimelist.net/v2/anime/${id}?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics`
--- store/Manga/Manga-Actions.ts
+++ store/Manga/Manga-Actions.ts
@@ -1,47 +1,47 @@
-import { MangaActions } from './Manga-Slice';
+import { MangaActions } from "./Manga-Slice";
 
-const CLIENT_ID = '4b75af5e9466c3b143191b860c40c4eb';
+const CLIENT_ID = "4b75af5e9466c3b143191b860c40c4eb";
 
-export const getData = (type: string) => {
-	return async (dispatch: any) => {
-		dispatch(MangaActions.setLoader({ choice: type, loading: true }));
+export const getMangaData = (type: string) => {
+  return async (dispatch: any) => {
+    dispatch(MangaActions.setLoader({ choice: type, loading: true }));
 
-		const getData = async () => {
-			const res = await fetch(
-				`https://api.myanimelist.net/v2/manga/ranking?ranking_type=${type}`,
-				{
-					headers: { 'X-MAL-Client-ID': CLIENT_ID }
-				}
-			);
-			if (res.status !== 200) {
-				throw new Error(`Failed to fetch Top ${type}`);
-			}
-			return res.json();
-		};
+    const getData = async () => {
+      const res = await fetch(
+        `https://api.myanimelist.net/v2/manga/ranking?ranking_type=${type}`,
+        {
+          headers: { "X-MAL-Client-ID": CLIENT_ID },
+        }
+      );
+      if (res.status !== 200) {
+        throw new Error(`Failed to fetch Top ${type}`);
+      }
+      return res.json();
+    };
 
-		try {
-			const result = await getData();
-			// switch (type) {
-			// 	case 'manga':
-			// 		dispatch(MangaActions.setTopManga(result.data))
-			// 		break;
-			// 	case 'novels':
-			// 		dispatch(MangaActions.setTopNovels(result.data));
-			// 		break;
-			// 	case 'manhua':
-			// 		dispatch(MangaActions.setTopManhua(result.data));
-			// 		break;
-			// 	case 'manhwa':
-			// 		dispatch(MangaActions.setTopManhwa(result.data));
-			// 		break;
-			// 	case 'bypopularity':
-			// 		dispatch(MangaActions.setMostPopular(result.data));
-			// 		break;
-			// }
-			dispatch(MangaActions.setLoader({ choice: type, loading: false }));
-		} catch (err) {
-			dispatch(MangaActions.setLoader({ choice: type, loading: false }));
-			console.log(err);
-		}
-	};
+    try {
+      const result = await getData();
+      // switch (type) {
+      // 	case 'manga':
+      // 		dispatch(MangaActions.setTopManga(result.data))
+      // 		break;
+      // 	case 'novels':
+      // 		dispatch(MangaActions.setTopNovels(result.data));
+      // 		break;
+      // 	case 'manhua':
+      // 		dispatch(MangaActions.setTopManhua(result.data));
+      // 		break;
+      // 	case 'manhwa':
+      // 		dispatch(MangaActions.setTopManhwa(result.data));
+      // 		break;
+      // 	case 'bypopularity':
+      // 		dispatch(MangaActions.setMostPopular(result.data));
+      // 		break;
+      // }
+      dispatch(MangaActions.setLoader({ choice: type, loading: false }));
+    } catch (err) {
+      dispatch(MangaActions.setLoader({ choice: type, loading: false }));
+      console.log(err);
+    }
+  };
 };
