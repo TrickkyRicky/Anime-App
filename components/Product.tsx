c4537d467ce70fdd8d8d37e124e2b8821180a6f7
trickkyricky <rickygyoungjr@gmail.com>
2022-01-07 14:17:08 -0600

    Api calls complete | grid tweaks

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,36 +1,44 @@
-import React from 'react';
-import { Text, VStack } from 'native-base';
-import { Dimensions, Image } from 'react-native';
+import React from "react";
+import { Text, VStack } from "native-base";
+import { Dimensions, Image, View } from "react-native";
 
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
-const Product = ({ name, image }) => {
-	return (
-		<VStack alignItems='center' px={1}>
-			<Image
-				source={{
-					uri: image
-				}}
-				style={{
-					borderRadius: 15,
-					width: screenWidth * 0.45,
-					height: screenHeight * 0.3,
-					resizeMode: 'cover'
-				}}
-			/>
+interface ProductProps {
+  title: string;
+  image: string;
+}
 
-			<Text
-				color='#fff'
-				fontWeight={500}
-				fontFamily={'mont'}
-				fontSize='lg'
-				mb={3}
-			>
-				{name}
-			</Text>
-		</VStack>
-	);
+const Product = ({ title, image }: ProductProps) => {
+  return (
+    <View>
+      {/* <VStack alignItems="center" px={1} style={{ width: "100%" }}> */}
+      <Image
+        source={{
+          uri: image,
+        }}
+        style={{
+          borderRadius: 15,
+          width: "100%",
+          height: 250,
+          resizeMode: "cover",
+        }}
+      />
+
+      <Text
+        color="#fff"
+        fontWeight={500}
+        fontFamily={"mont"}
+        fontSize="lg"
+        numberOfLines={2}
+        mb={3}
+      >
+        {title}
+      </Text>
+      {/* </VStack> */}
+    </View>
+  );
 };
 
 export default Product;
--- screens/Anime/TopAiringAnime.tsx
+++ screens/Anime/TopAiringAnime.tsx
@@ -1,122 +1,82 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Box, HStack, Text, VStack } from 'native-base';
-import { getAnimeData } from '../../store/Anime/Anime-Actions';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Box, HStack, Text, VStack } from "native-base";
+import { getAnimeData } from "../../store/Anime/Anime-Actions";
 
-import Product from '../../components/Product';
-import Carousel from '../../components/Carousel';
-import { ScrollView, TouchableOpacity, FlatList, View } from 'react-native';
-import { AnimeActions } from '../../store/Anime/Anime-Slice';
-import { RootState } from '../../store/index';
+import Product from "../../components/Product";
+import Carousel from "../../components/Carousel";
+import { ScrollView, TouchableOpacity, FlatList, View } from "react-native";
+import { AnimeActions } from "../../store/Anime/Anime-Slice";
+import { RootState } from "../../store/index";
 
 interface ListingItem {
-	item: {
-		node: {
-			id: number;
-			main_picture: {
-				large: string;
-				medium: string;
-			};
-			title: string;
-		};
-	};
+  item: {
+    node: {
+      id: number;
+      main_picture: {
+        large: string;
+        medium: string;
+      };
+      title: string;
+    };
+  };
 }
-const data = [
-	{
-		id: 1,
-		image: 'https://api-cdn.myanimelist.net/images/anime/6/44297.jpg',
-		name: 'One Piece'
-	},
-	{
-		id: 2,
-		image: 'https://api-cdn.myanimelist.net/images/anime/1259/110227.jpg',
-		name: 'Holo no Graffiti'
-	},
-	{
-		id: 3,
-		image: 'https://api-cdn.myanimelist.net/images/anime/6/44297.jpg',
-		name: 'One Piece'
-	},
-	{
-		id: 4,
-		image: 'https://api-cdn.myanimelist.net/images/anime/1259/110227.jpg',
-		name: 'Holo no Graffiti'
-	},
-	{
-		id: 5,
-		image: 'https://api-cdn.myanimelist.net/images/anime/6/44297.jpg',
-		name: 'One Piece'
-	},
-	{
-		id: 6,
-		image: 'https://api-cdn.myanimelist.net/images/anime/1259/110227.jpg',
-		name: 'Holo no Graffiti'
-	},
-	{
-		id: 7,
-		image: 'https://api-cdn.myanimelist.net/images/anime/6/44297.jpg',
-		name: 'One Piece'
-	},
-	{
-		id: 8,
-		image: 'https://api-cdn.myanimelist.net/images/anime/1259/110227.jpg',
-		name: 'Holo no Graffiti'
-	}
-];
 
 const TopAiringAnime = () => {
-	const dispatch = useDispatch();
-	const topAiring = useSelector(
-		(state: RootState) => state.Anime.topAiring.anime
-	);
-	console.log(topAiring.length);
-	useEffect(() => {
-		dispatch(AnimeActions.setTopAiringReset());
-		dispatch(getAnimeData('airing'));
-	}, []);
+  const dispatch = useDispatch();
+  const topAiring = useSelector(
+    (state: RootState) => state.Anime.topAiring.anime
+  );
+  useEffect(() => {
+    dispatch(AnimeActions.setTopAiringReset());
+    dispatch(getAnimeData("airing"));
+  }, []);
 
-	return (
-		<Box flex={1} bg='#3730a3' p={2}>
-			<VStack mb={12} alignItems='center'>
-				{/* <Text
-					color='#fff'
-					textAlign='center'
-					fontSize='3xl'
-					fontWeight={700}
-					mb={2}
-					fontFamily={'mont-bold'}
-				>
-					Top Airing Anime
-				</Text> */}
+  return (
+    <View style={{ width: "100%" }}>
+      {/* <Box flex={1} bg="#3730a3" p={2} style={{ width: "100%" }}>
+      <VStack mb={12} alignItems="center"> */}
+      <Text
+        color="#fff"
+        textAlign="center"
+        fontSize="3xl"
+        fontWeight={700}
+        mb={2}
+        fontFamily={"mont-bold"}
+      >
+        Top Airing Anime
+      </Text>
 
-				<FlatList
-					data={topAiring}
-					keyExtractor={(item: ListingItem['item']) => item.node.id.toString()}
-					renderItem={({ item }) => {
-						const anime = item.node;
-						return (
-							<TouchableOpacity
-								onPress={() => console.log(`${anime.title} pressed`)}
-							>
-								<Product
-									name={anime.title}
-									image={anime?.main_picture?.medium}
-								/>
-							</TouchableOpacity>
-						);
-					}}
-					horizontal={false}
-					ListHeaderComponent={() => <Carousel />}
-					// showsVerticalScrollIndicator={false}
-					showsHorizontalScrollIndicator={false}
-					numColumns={2}
-					ItemSeparatorComponent={() => (
-						<View style={{ paddingHorizontal: 2 }} />
-					)}
-				/>
-			</VStack>
-		</Box>
-	);
+      <FlatList
+        data={topAiring}
+        keyExtractor={(item: ListingItem["item"]) => item.node.id.toString()}
+        renderItem={({ item }) => {
+          const anime = item.node;
+          return (
+            <TouchableOpacity
+              style={{ width: "50%", paddingHorizontal: 10 }}
+              onPress={() => console.log(`${anime.title} pressed`)}
+            >
+              <Product
+                title={anime.title}
+                image={anime?.main_picture?.medium}
+              />
+            </TouchableOpacity>
+          );
+        }}
+        horizontal={false}
+        ListHeaderComponent={() => <Carousel />}
+        showsHorizontalScrollIndicator={false}
+        numColumns={2}
+        // showsVerticalScrollIndicator={false}
+        //   ItemSeparatorComponent={() => (
+        //     <View style={{ paddingHorizontal: 2 }} />
+        //   )}
+      />
+      {/* </VStack>
+    </Box> */}
+    </View>
+  );
 };
 
 export default TopAiringAnime;
