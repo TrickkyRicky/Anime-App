362fce1d61f6b0975dd4c109daaebaacb4c08814
trickkyricky <rickygyoungjr@gmail.com>
2022-01-19 14:03:32 -0600

    DetailsInfo component is much more readable

--- components/Details/DetailSubTItle.tsx
+++ components/Details/DetailSubTItle.tsx
@@ -1,34 +1,75 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Box, Text } from 'native-base';
+import React, { Fragment } from "react";
+import { View, useColorScheme } from "react-native";
+import { Box, Text } from "native-base";
 
 type Props = {
-	title: string;
-	info: string;
-	colorScheme: string | null | undefined;
+  title: string;
+  info: string | [];
+};
+type Studio = {
+  id: number;
+  name: String;
 };
 
-const DetailSubTitle = ({ colorScheme, title, info }: Props) => {
-	return (
-		<Box flexDirection='row'>
-			<Text
-				mb={1}
-				fontFamily={'mont-bold'}
-				color={colorScheme === 'dark' ? '#654582' : '#52376A'}
-				fontSize='sm'
-			>
-				{title}
-			</Text>
-			<Text
-				fontFamily={'mont-bold'}
-				color={colorScheme === 'dark' ? '#8e8e8e' : '#414141'}
-				fontSize='sm'
-				pl={1.5}
-			>
-				{info}
-			</Text>
-		</Box>
-	);
+const DetailSubTitle = ({ title, info }: Props) => {
+  const colorScheme = useColorScheme();
+  let infoChange = null;
+
+  if (Array.isArray(info)) {
+    infoChange = info.map((item: Studio, index) => {
+      return (
+        <Fragment key={item.id}>
+          <Text>{index ? "," : ""}</Text>
+          <Text
+            fontFamily={"mont-bold"}
+            color={colorScheme === "dark" ? "#8e8e8e" : "#414141"}
+            fontSize="sm"
+            pl={1.5}
+          >
+            {item.name}
+          </Text>
+        </Fragment>
+      );
+    });
+
+    if (!info.length) {
+      infoChange = (
+        <Text
+          fontFamily={"mont-bold"}
+          color={colorScheme === "dark" ? "#8e8e8e" : "#414141"}
+          fontSize="sm"
+          pl={1.5}
+        >
+          Unknown
+        </Text>
+      );
+    }
+  } else {
+    infoChange = (
+      <Text
+        fontFamily={"mont-bold"}
+        color={colorScheme === "dark" ? "#8e8e8e" : "#414141"}
+        fontSize="sm"
+        pl={1.5}
+      >
+        {info}
+      </Text>
+    );
+  }
+
+  return (
+    <Box flexDirection="row">
+      <Text
+        mb={1}
+        fontFamily={"mont-bold"}
+        color={colorScheme === "dark" ? "#654582" : "#52376A"}
+        fontSize="sm"
+      >
+        {title}
+      </Text>
+      {infoChange}
+    </Box>
+  );
 };
 
 export default DetailSubTitle;
--- components/Details/DetailsInfo.tsx
+++ components/Details/DetailsInfo.tsx
@@ -1,121 +1,91 @@
-import React from 'react';
-import { Heading, HStack, Box, Text } from 'native-base';
-import { View, ScrollView, useColorScheme } from 'react-native';
-import DetailSubTitle from './DetailSubTItle';
-import * as Animatable from 'react-native-animatable';
+import React from "react";
+import { Heading, HStack, Box, Text } from "native-base";
+import { View, ScrollView, useColorScheme } from "react-native";
+import DetailSubTitle from "./DetailSubTItle";
+import * as Animatable from "react-native-animatable";
 
 type Props = {
-	width: number;
-	height: number;
-	spacing: number;
-	animeDetails: {
-		details: any;
-		loader: boolean;
-	};
-	animeTitle: string;
+  width: number;
+  height: number;
+  spacing: number;
+  animeDetails: {
+    details: any;
+    loader: boolean;
+  };
+  animeTitle: string;
 };
 
 type studio = {
-	id: number;
-	name: string;
+  id: number;
+  name: string;
 };
 
 const DetailsInfo = ({
-	width,
-	height,
-	spacing,
-	animeDetails,
-	animeTitle
+  width,
+  height,
+  spacing,
+  animeDetails,
+  animeTitle,
 }: Props) => {
-	const colorScheme = useColorScheme();
-	return (
-		<View
-			style={{
-				backgroundColor: colorScheme === 'dark' ? '#121212' : '#ffffff',
-				position: 'absolute',
-				width: width,
-				height: height,
-				transform: [{ translateY: height / 2 }],
-				borderRadius: 32,
-				padding: spacing,
-				paddingTop: 32
-			}}
-		>
-			<Animatable.View animation='fadeInUp' delay={200}>
-				<Heading
-					size='xl'
-					fontFamily={'mont-bold'}
-					color={colorScheme === 'dark' ? '#654582' : '#52376A'}
-					numberOfLines={2}
-					mb={1}
-				>
-					{animeTitle}
-				</Heading>
-				<ScrollView
-					showsVerticalScrollIndicator={false}
-					style={{ height: height * 0.35 }}
-					alwaysBounceVertical={true}
-					bounces={true}
-				>
-					<DetailSubTitle
-						colorScheme={colorScheme}
-						title='Airing Date:'
-						info={
-							!animeDetails?.details?.start_date
-								? 'No Date Available'
-								: animeDetails?.details?.start_date
-						}
-					/>
+  const colorScheme = useColorScheme();
+  return (
+    <View
+      style={{
+        backgroundColor: colorScheme === "dark" ? "#121212" : "#ffffff",
+        position: "absolute",
+        width: width,
+        height: height,
+        transform: [{ translateY: height / 2 }],
+        borderRadius: 32,
+        padding: spacing,
+        paddingTop: 32,
+      }}
+    >
+      <Animatable.View animation="fadeInUp" delay={200}>
+        <Heading
+          size="xl"
+          fontFamily={"mont-bold"}
+          color={colorScheme === "dark" ? "#654582" : "#52376A"}
+          numberOfLines={2}
+          mb={1}
+        >
+          {animeTitle}
+        </Heading>
 
-					<Box flexDirection='row'>
-						<Text
-							mb={1}
-							fontFamily={'mont-bold'}
-							color={colorScheme === 'dark' ? '#654582' : '#52376A'}
-							fontSize='sm'
-						>
-							Studio:
-						</Text>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={{ height: height * 0.35 }}
+          alwaysBounceVertical={true}
+          bounces={true}
+        >
+          <DetailSubTitle
+            title="Airing Date:"
+            info={
+              !animeDetails?.details?.start_date
+                ? "No Date Available"
+                : animeDetails?.details?.start_date
+            }
+          />
+          <DetailSubTitle
+            title="Studio:"
+            info={animeDetails?.details?.studios}
+          />
 
-						{!animeDetails.details.studios.length ? (
-							<Text
-								fontFamily={'mont-bold'}
-								color={colorScheme === 'dark' ? '#8e8e8e' : '#414141'}
-								fontSize='sm'
-								pl={1.5}
-							>
-								Unknown
-							</Text>
-						) : (
-							animeDetails?.details?.studios?.map((studio: studio) => (
-								<Text
-									key={studio.id}
-									fontFamily={'mont-bold'}
-									color={colorScheme === 'dark' ? '#8e8e8e' : '#414141'}
-									fontSize='sm'
-									pl={1.5}
-								>
-									{studio.name}
-								</Text>
-							))
-						)}
-					</Box>
-
-					<Text
-						fontSize='sm'
-						fontFamily={'mont'}
-						color={colorScheme === 'dark' ? '#654582' : '#52376A'}
-						textAlign='justify'
-					>
-						{animeDetails?.details?.synopsis != ''
-							? animeDetails?.details?.synopsis
-							: 'No Description Available'}
-					</Text>
-				</ScrollView>
-				<View style={{ height: 5 }} />
-			</Animatable.View>
-		</View>
-	);
+          <Text
+            fontSize="sm"
+            fontFamily={"mont"}
+            color={colorScheme === "dark" ? "#654582" : "#52376A"}
+            textAlign="justify"
+          >
+            {animeDetails?.details?.synopsis != ""
+              ? animeDetails?.details?.synopsis
+              : "No Description Available"}
+          </Text>
+        </ScrollView>
+        <View style={{ height: 5 }} />
+      </Animatable.View>
+    </View>
+  );
 };
 // genres []
 // broadcast ---> broadcast {}
